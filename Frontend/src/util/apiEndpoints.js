085e5f0a920fc25c7b74062bee94f3692f4ca150
sunilthakor143 <sjthakor6787@gmail.com>
2026-07-11 12:07:32 +0530

    Refactor UserCreditsContext and apiEndpoints to improve code clarity and maintainability

--- Frontend/src/util/apiEndpoints.js
+++ Frontend/src/util/apiEndpoints.js
@@ -4,7 +4,6 @@ const BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://cloud-share-app-
 export const apiEndpoints = {
     FETCH_FILES: `${BASE_URL}/files/my`,
     GET_CREDITS: `${BASE_URL}/users/credits`,
-    GET_CREDITS: () => api_get("/users/credits"),
     TOGGLE_FILE: (id) => `${BASE_URL}/files/${id}/toggle-public`,
     DOWNLOAD_FILE: (id) => `${BASE_URL}/files/download/${id}`,
     DELETE_FILE: (id) => `${BASE_URL}/files/${id}`,
